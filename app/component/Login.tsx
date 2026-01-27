adfd6ee3926f3a58f744e6909b59424ba864420e
juninho <hei.juninho.2@gmail.com>
2026-01-27 19:43:15 +0300

    feat(login): add state management for login page

--- app/component/Login.tsx
+++ app/component/Login.tsx
@@ -1,11 +1,55 @@
-import { Text, TextInput, View } from "react-native";
+import { loginStyle } from "@/style/login-style";
+import { useState } from "react";
+import { Alert, Button, Text, TextInput, View } from "react-native";
 
 export default function Login(){
+    const [userName, setUserName] =  useState("");
+    const [password, setPassword] =  useState("");
+    const [loading, setLoading] = useState(false);
+
+    // fonction en anglais le message
+    const handleLogin = async () => {
+        if (!userName || !password) {
+          Alert.alert("Error", "Please enter both username and password");
+          return;
+        }
+    
+        setLoading(true);
+    
+        try {
+          const response = await fetch("https://vendredi-soir-posu.onrender.com/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              userName,
+              password,
+            }),
+          });
+    
+          if (!response.ok) {
+            throw new Error("Login failed. Please check your credentials.");
+          }
+    
+          const data = await response.json();
+          console.log("Login success", data);
+    
+          // data contient UserWithApiKey
+          Alert.alert("success", `Welcome ${data.username}`);
+          // Ici tu peux sauvegarder data.apiKey dans AsyncStorage pour les futures requêtes
+        } catch (error: any) {
+          Alert.alert("Error", error.message);
+        } finally {
+          setLoading(false);
+        }
+      };
     return (<View>
-        <Text className="title">Login</Text>
+        <Text style={loginStyle.title}>Login</Text>
         <Text>user name</Text>
-        <TextInput style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}/>
+        <TextInput style={loginStyle.input} onChangeText={setUserName}/>
         <Text>password</Text>
-        <TextInput secureTextEntry={true} style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}/>
+        <TextInput secureTextEntry={true} style={loginStyle.input} onChangeText={setPassword}/>
+        <Button title={loading ? "loading ..." : "Login"} onPress={handleLogin}/>
     </View>)
 }
---
+++ style/login-style.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const loginStyle = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    width: 200,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+  },
+});
